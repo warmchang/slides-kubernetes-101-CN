0e8abff8c4c5e176c3e6c73c481463f11779d1fe
William Zhang <warmchang@outlook.com>
2019-01-22 19:24:29 +0800

    Make the slides-kubernetes-101 release ⚓
    Signed-off-by: William Zhang <zhang.wanmin@zte.com.cn>

--- make-release-zip.sh
+++ make-release-zip.sh
@@ -6,21 +6,26 @@ set -o pipefail
 
 rm -rf release
 
-ZIPNAME="slides-kubernetes-101-$(date "+%Y%m%d")"
+ZIPNAME="slides-kubernetes-101-zh-cn-$(date "+%Y%m%d")"
 mkdir -p release/$ZIPNAME
 
 mkdir -p release/$ZIPNAME/fonts
 cp fonts/* release/$ZIPNAME/fonts
+mkdir -p release/$ZIPNAME/photos
+cp photos/*.jpg release/$ZIPNAME/photos/
 mkdir -p release/$ZIPNAME/img
-cp img/*.jpg release/$ZIPNAME/img/
+cp img/*.png release/$ZIPNAME/img/
 mkdir -p release/$ZIPNAME/diagrams
 cp diagrams/*.svg release/$ZIPNAME/diagrams/
 mkdir -p release/$ZIPNAME/js
 cp js/* release/$ZIPNAME/js/
 
-cp kubernetes-101.html release/$ZIPNAME/
+cp index.html release/$ZIPNAME/
+cp slides.md release/$ZIPNAME/
 cp LICENSE.md release/$ZIPNAME/
 cp README.md release/$ZIPNAME/
 
 cd release
-zip -r $ZIPNAME.zip $ZIPNAME
+# zip -r $ZIPNAME.zip $ZIPNAME
+tar -zcf $ZIPNAME.tar.gz $ZIPNAME
+rm -rf $ZIPNAME
